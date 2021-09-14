c7b365ee19c50fa3c2e3f2a4eb3491ec38230bd4
NATFINEST <ese.nat4@gmail.com>
2021-09-14 12:37:23 +0100

    Simple Node Server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+const LISTINGS_DATA_FILE = path.join(__dirname, 'server-todos.json');
+
+app.set('port', process.env.PORT || 8000);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/todos', (_req, res) => {
+  setTimeout(() => {
+    fs.readFile(LISTINGS_DATA_FILE, (err, data) => {
+      res.setHeader('Cache-Control', 'no-cache');
+      res.json(JSON.parse(data));
+    });
+  }, 1000);
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`);
+});
